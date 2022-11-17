3bfa0cfb5d91ed6896d23f59d41f8d0ba012791f
Kelvin Oliveira <63024154+KelvinSilvaDev@users.noreply.github.com>
2022-11-17 16:18:54 -0300

    fix: db connection fix

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  `mongodb+srv://admin:1998*Kel@notepadcluster.vhrmyaj.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://admin:1998*Kel@notepadcluster.vhrmyaj.mongodb.net/notedb`
 );
 
 let db = mongoose.connection;
