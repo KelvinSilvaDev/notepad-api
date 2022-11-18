f0d63d524ec9da98e88782b46702236b74572fbe
Kelvin Oliveira <63024154+KelvinSilvaDev@users.noreply.github.com>
2022-11-17 21:32:12 -0300

    cors use cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 
@@ -9,6 +10,7 @@ db.once("open", function () {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log("Request received");
   res.header("Access-Control-Allow-Origin", "*");
